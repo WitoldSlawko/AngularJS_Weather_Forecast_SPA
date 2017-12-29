61a9e226caab6fe10cdbf4a8ca710fe2aabd5fa2
WitoldSlawko <witold.slawko@gmail.com>
2017-12-29 13:09:51 +0100

    changed to https

--- services/cityService.js
+++ services/cityService.js
@@ -2,7 +2,7 @@
 
 weatherApp.service('cityService', ['$http', function ($http) {
     this.getWeather = function(cityId) {
-        return $http.get(`http://api.openweathermap.org/data/2.5/forecast`,
+        return $http.get(`https://api.openweathermap.org/data/2.5/forecast`,
         { params: { id: cityId, APPID: '75c1af1ffe36d572b36e6421218eab9e' } })
         .success(function (data) {
             return data;
